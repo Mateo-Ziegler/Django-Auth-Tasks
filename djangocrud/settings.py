c5dbbbe263c00cbadfa58303457749c344f4be4c
Mateo Ziegler <zieglermateol@gmail.com>
2026-01-04 13:25:29 -0300

    Fix ALLOWED_HOSTS for Render

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-c_usg8b4ss^=k+kzj&!2jj%s26$%zt28mumcvs5$)y7k=j0pmm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "django-tasks-with-login-and-register.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
+
 
 
 # Application definition
